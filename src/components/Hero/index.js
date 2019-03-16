2ccd15da07a9dae2164a869c700681fd096a1b72
OP <op@willowcreative.io>
2019-03-16 10:40:31 -0700

    ssad

---
+++ src/components/Hero/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Carousel from 'nuka-carousel';
+
+const Hero = ({ items }) => (
+  <ul className="hero">
+    <Carousel>
+      {items.map((item) => (
+        <div key={i} className="wrapper" style={{ backgroundImage: `url(${item.image.childImageSharp.fluid.src})` }}>
+          <div className="window-centered">
+            <h3>{item.subtitle}</h3>
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+          </div>
+        </div>
+      ))}
+    </Carousel>
+  </ul>
+)
+
+export default Hero
