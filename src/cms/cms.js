a0e031ce0f068e12c305797d0753c6b921ecf2c6
OP <op@willowcreative.io>
2019-03-16 10:35:33 -0700

    cmssss

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,12 @@
 import CMS from 'netlify-cms'
+import styles from '!css-loader!sass-loader!../components/all.sass'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
+CMS.registerPreviewStyle(styles.toString(), { raw: true })
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
