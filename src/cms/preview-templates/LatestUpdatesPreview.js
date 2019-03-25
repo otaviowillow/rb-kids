40408bc9c64ab667be01cfdfe2635a5b39fdcf48
OP <op@willowcreative.io>
2019-03-25 12:45:58 -0700

    Removing layout

--- src/cms/preview-templates/LatestUpdatesPreview.js
+++ src/cms/preview-templates/LatestUpdatesPreview.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import { LatestUpdatesTemplate } from '../../templates/latest-updates'
-import Layout from '../../components/Layout'
 
 const LatestUpdatesPreview = ({ entry, widgetFor, getAsset }) => {
   return (
-    <Layout>
-      <LatestUpdatesTemplate
-        title={entry.getIn(['data', 'title'])}
-        date={entry.getIn(['data', 'date'])}
-        background={getAsset(entry.getIn(['data', 'background']))}
-        html={widgetFor('body')}
-      />
-    </Layout>
+    <LatestUpdatesTemplate
+      title={entry.getIn(['data', 'title'])}
+      date={entry.getIn(['data', 'date'])}
+      background={getAsset(entry.getIn(['data', 'background']))}
+      html={widgetFor('body')}
+    />
   )
 }
 
--- src/cms/preview-templates/ProgramsPreview.js
+++ src/cms/preview-templates/ProgramsPreview.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { ProgramsTemplate } from '../../templates/programs'
-import Layout from '../../components/Layout'
 
 const ProgramsPreview = ({ entry, widgetFor, getAsset }) => {
   return (
-    <Layout>
-      <ProgramsTemplate
-        title={entry.getIn(['data', 'title'])}
-        background={getAsset(entry.getIn(['data', 'background']))}
-        html={widgetFor('body')}
-      />
-    </Layout>
+    <ProgramsTemplate
+      title={entry.getIn(['data', 'title'])}
+      background={getAsset(entry.getIn(['data', 'background']))}
+      html={widgetFor('body')}
+    />
   )
 }
 
