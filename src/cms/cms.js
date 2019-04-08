d16104e8711e07aae0e0a695f3427cf188584d26
OP <op@willowcreative.io>
2019-04-07 17:57:59 -0700

    Enrolment

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,7 @@ import styles from '!css-loader!sass-loader!../components/all.sass'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import PoliciesPagePreview from './preview-templates/PoliciesPagePreview'
+import EnrollmentPreview from './preview-templates/EnrollmentPreview'
 import PhotoGalleryPreview from './preview-templates/PhotoGalleryPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import LatestUpdatesPreview from './preview-templates/LatestUpdatesPreview'
@@ -12,6 +13,7 @@ import EmployeesPreview from './preview-templates/EmployeesPreview'
 CMS.registerPreviewStyle(styles.toString(), { raw: true })
 CMS.registerPreviewTemplate('index-page', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('enrollment', EnrollmentPreview)
 CMS.registerPreviewTemplate('policies', PoliciesPagePreview)
 CMS.registerPreviewTemplate('photo-gallery', PhotoGalleryPreview)
 CMS.registerPreviewTemplate('latest-updates', LatestUpdatesPreview)
